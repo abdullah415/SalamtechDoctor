daf87fdffc2f1087eaebf82a96016007f5877c2a
AbdullahAbbas08 <34220789+AbdullahAbbas08@users.noreply.github.com>
2021-12-21 17:46:20 +0200

    Apply Localization Init , Still there Bug at First

--- src/app/Shared/nav/nav.component.ts
+++ src/app/Shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,8 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+//#region Declare Variables
+DefaultLang!:string;
+//#endregion
 
+  //#region  Constructor
+  constructor(private translate:TranslateService){ 
+    // translate.setDefaultLang('en');
+    translate.use('en');
+   }
+  //#endregion
+
+  //#region On Init Method
   ngOnInit(): void {
+
+    //#region Init Variables Scope
+    this.DefaultLang = "en";
+    //#endregion
+
   }
+  //#endregion
+
+
+ 
+
+  //#region change Language Method
+  ChangeLanguage(e:string)
+  {
+    if(e == 'en')
+    {
+      this.translate.use(e);
+      document.getElementsByTagName('html')[0].setAttribute("dir","rtl");
+      this.DefaultLang = 'ar';
+    }
+    if(e == 'ar')
+    {
+      this.translate.use(e);
+      document.getElementsByTagName('html')[0].setAttribute("dir","ltr");
+      this.DefaultLang = 'en';
+    }
+
+  }
+  //#endregion
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Shared/nav/nav.component';
 import { FooterComponent } from './Shared/footer/footer.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// AoT requires an exported function for factories
+export function CreateTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http,'../assets/i18n/','.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: CreateTranslateLoader,
+          deps: [HttpClient]
+      },
+      defaultLanguage:'en'
+  })
+],
   providers: [],
   bootstrap: [AppComponent]
 })
