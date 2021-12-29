3459e3f356b4b8160c2fee240ba347c9c912e0b1
AbdullahAbbas08 <34220789+AbdullahAbbas08@users.noreply.github.com>
2021-12-29 15:30:02 +0200

    sidebar main style

--- src/Service/signup/responsesignup.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Responsesignup } from './responsesignup';
-
-describe('Responsesignup', () => {
-  it('should create an instance', () => {
-    expect(new Responsesignup()).toBeTruthy();
-  });
-});
