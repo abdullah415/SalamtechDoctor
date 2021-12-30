497cc4b8dc36b9693f423c40c5347517d61d97d5
abdullah415 <76951284+abdullah415@users.noreply.github.com>
2021-12-30 16:05:52 +0200

    Last

--- src/Service/Doctor/doctor.service.ts
+++ src/Service/Doctor/doctor.service.ts
@@ -5,14 +5,14 @@ import { environment } from 'src/environments/environment';
 import { DropDownModel } from 'src/Models/drop-down-model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' 
 })
 export class DoctorService {
 
   constructor(private http: HttpClient) { }
 
   //#region Options
-  httpOptions = { 
+  httpOptions = {
     headers: new HttpHeaders({
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjAxMDExMTExMTExIiwibmFtZWlkIjoiMTc0IiwianRpIjoiZmZkNmQ4MjItMmUyOS00ZGFhLTk5M2MtYzRmYmRkOTI1NWY4IiwiZXhwIjoxNjQxMDQxNDM5LCJpc3MiOiJTYWxhbVRlY2hAMjAyMSIsImF1ZCI6IlNhbGFtVGVjaEAyMDIxIn0.cd_fGmg73iV9o3d4r0Rke0mNSjXO8O7gg8wHF-SihDU'
       }
