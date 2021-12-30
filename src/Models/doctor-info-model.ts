06b6666d879a04d29d8ed43cfd98ae7f50634349
AbdullahAbbas08 <34220789+AbdullahAbbas08@users.noreply.github.com>
2021-12-30 18:01:14 +0200

    doctor info api 1.2

--- src/Models/doctor-info-model.ts
+++ src/Models/doctor-info-model.ts
@@ -1,3 +1,19 @@
 export class DoctorInfoModel {
-    
+
+    FirstName: string;
+    FirstNameAr: string;
+    MiddelName: string;
+    MiddelNameAr: string;
+    LastName: string;
+    LastNameAr: string;
+    GenderId: number;
+    SpecialistId: number;
+    SeniorityLevelId: number;
+    NationalityId: number;
+    DoctorSubSpecialist: number;
+    Birthday: string;
+    Website: string;
+    DoctorInfo: string;
+    DoctorInfoAr: string;
+    profileImage: File;
 }
--- src/app/Components/Signup/doctor-info/doctor-info.component.ts
+++ src/app/Components/Signup/doctor-info/doctor-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { DoctorInfoModel } from 'src/Models/doctor-info-model';
 import { DropDownModel } from 'src/Models/drop-down-model';
 import { IdNameList } from 'src/Models/id-name-list';
 import { DoctorService } from 'src/Service/Doctor/doctor.service';
@@ -17,7 +18,11 @@ export class DoctorInfoComponent implements OnInit {
   //#region  Declare Variables
   DoctorInfoForm : FormGroup ;
   DropDownModel:DropDownModel;
-  DropDownList:IdNameList[];
+  DoctorInfoModel:DoctorInfoModel;
+  DropDownList_Speciality:IdNameList[];
+  DropDownList_SubSpeciality:IdNameList[];
+  DropDownList_SeniorityLevel:IdNameList[];
+  DropDownList_GetCountries:IdNameList[];
   //#endregion
 
   //#region Constructor
@@ -28,67 +33,210 @@ export class DoctorInfoComponent implements OnInit {
   //#endregion
 
   //#region On Init Method
-ngOnInit(): void {
+    ngOnInit(): void {
 
-  //#region Init Values
-  document.getElementById('Doctorinfo')?.classList.add('OnClick-Style');
-  document.getElementById('Signup')?.classList.add('OnClick-Style');
-  this.DropDownList = [];
-  // this.DropDownModel.Data = [];
+      //#region Init Values
+      document.getElementById('Doctorinfo')?.classList.add('OnClick-Style');
+      document.getElementById('Signup')?.classList.add('OnClick-Style');
+      this.DropDownList_Speciality = [];
+      // this.DoctorInfoForm.controls.ImageDoctor.value = "";
+      // this.DropDownModel.Data = [];
+      //#endregion
+
+      //#region  Register Form Section
+      this.DoctorInfoForm = this.fb.group(
+        {
+            FirstName:['',[Validators.required , Validators.minLength(3)]],
+            FirstNameAr:['',[Validators.required , Validators.minLength(3)]],
+            MiddleName:['',[Validators.minLength(3) , Validators.required]],
+            MiddleNameAr:['',[Validators.minLength(3) , Validators.required]],
+            LastName:['',[Validators.minLength(3) , Validators.required]],
+            LastNameAr:['',[Validators.minLength(3) , Validators.required]],
+            ImageDoctor:['',[ Validators.required]],
+            Email:['',[Validators.email , Validators.required]],
+            PhoneNumber:['',[Validators.required, Validators.min(1000000000), Validators.max(9999999999)]],
+            Password:['',[Validators.required , Validators.minLength(6)]],
+            ConfirmPassword:['',[Validators.required ]],
+            Gender:['',[Validators.required ]],
+            Nationality:['',[Validators.required ]],
+            Country:['',[Validators.required ]],
+            DateOfBirth:['',[Validators.required ]],
+            Speciality:['',[Validators.required]],
+            SubSpeciality:['',[Validators.required]],
+            Seniority:['',[Validators.required]],
+            BiographyAr:['',[Validators.required]],
+            Biography:['',[Validators.required]]
+          });
+      //#endregion
+
+      //#region call Methods 
+      this.GetSpecialistIdName('en');
+      this.SeniorityLevelIdName('en');
+      this.GetCountries('en');
+      //#endregion
+    }
+
+    //#endregion
+
+//#region Definition API's
+
+  //#region GetSubSpecialistIdName
+  GetSubSpecialistIdName(lang:string , specialListId:number)
+  {
+    this.DoctorService.GetSubSpecialistIdName(lang ,specialListId ).subscribe(
+      (response)=>{
+        this.DropDownList_SubSpeciality = response.Data;
+      },
+      (err)=>{
+        // console.log(err);
+      }
+    )
+  }
   //#endregion
 
-   //#region  Register Form Section
-   this.DoctorInfoForm = this.fb.group(
-    {
-        FirstName:['',[Validators.required , Validators.minLength(3)]],
-        FirstNameAr:['',[Validators.required , Validators.minLength(3)]],
-        MiddleName:['',[Validators.minLength(3) , Validators.required]],
-        MiddleNameAr:['',[Validators.minLength(3) , Validators.required]],
-        LastName:['',[Validators.minLength(3) , Validators.required]],
-        LastNameAr:['',[Validators.minLength(3) , Validators.required]],
-        Email:['',[Validators.email , Validators.required]],
-        PhoneNumber:['',[Validators.required, Validators.min(1000000000), Validators.max(9999999999)]],
-        Password:['',[Validators.required , Validators.minLength(6)]],
-        ConfirmPassword:['',[Validators.required ]],
-        Gender:['',[Validators.required ]],
-        Nationality:['',[Validators.required ]],
-        Country:['',[Validators.required ]],
-        DateOfBirth:['',[Validators.required ]],
-        Speciality:['',[Validators.required]],
-        SubSpeciality:['',[Validators.required]],
-        Seniority:['',[Validators.required]],
-        BiographyAr:['',[Validators.required]],
-        Biography:['',[Validators.required]]
-      });
+  //#region GetSubSpecialistIdName
+  CreateProfile(lang:string , _DoctorInfoModel:DoctorInfoModel)
+  {
+    this.DoctorService.CreateProfile(lang ,_DoctorInfoModel ).subscribe(
+      (response)=>{
+       console.log(response);
+      },
+      (err)=>{
+        console.log(err);
+      }
+    )
+  }
   //#endregion
 
-  //#region call Methods
-  this.DoctorService.GetSpecialistIdName('en').subscribe(
+  //#region  GetSpecialistIdName
+GetSpecialistIdName(lang:string)
+{
+  this.DoctorService.GetSpecialistIdName(lang).subscribe(
     (data)=>{
       this.DropDownModel = data;
-      this.DropDownList = this.DropDownModel.Data;
-      console.log(this.DropDownList);
+      this.DropDownList_Speciality = this.DropDownModel.Data;
+      // console.log(this.DropDownList);
     },
     (err)=>{}
   )
+}
+//#endregion
+
+  //#region GetSubSpecialistIdName
+  SeniorityLevelIdName(lang:string)
+  {
+    this.DoctorService.SeniorityLevelIdName(lang ).subscribe(
+      (response)=>{
+        this.DropDownList_SeniorityLevel = response.Data;
+      },
+      (err)=>{
+        // console.log(err);
+      }
+    )
+  }
   //#endregion
-}
+
+  //#region GetSubSpecialistIdName
+  GetCountries(lang:string)
+  {
+    this.DoctorService.GetCountries(lang ).subscribe(
+      (response)=>{
+        this.DropDownList_GetCountries = response.Data;
+      },
+      (err)=>{
+        // console.log(err);
+      }
+    )
+  }
+  //#endregion
+
 //#endregion
 
-  //#region changeStyle Method
-  DoctorImgSubmit()
-{
-  document.getElementById('Certificates')?.classList.add('OnClick-Style');
 
+  //#region Doctor Info Submit Method
+    DoctorInfoSubmit()
+  {
 
-}
-//#endregion
+    this.DoctorInfoModel.FirstName = this.DoctorInfoForm.controls.FirstName.value;
+    this.DoctorInfoModel.FirstNameAr = this.DoctorInfoForm.controls.FirstNameAr.value;
+    this.DoctorInfoModel.MiddelName = this.DoctorInfoForm.controls.MiddelName.value;
+    this.DoctorInfoModel.MiddelNameAr = this.DoctorInfoForm.controls.MiddelNameAr.value;
+    this.DoctorInfoModel.LastName = this.DoctorInfoForm.controls.LastName.value;
+    this.DoctorInfoModel.LastNameAr = this.DoctorInfoForm.controls.LastNameAr.value;
+    this.DoctorInfoModel.GenderId = this.DoctorInfoForm.controls.Gender.value;
+    this.DoctorInfoModel.NationalityId = this.DoctorInfoForm.controls.Country.value;
+    this.DoctorInfoModel.SeniorityLevelId = this.DoctorInfoForm.controls.Seniority.value;
+    this.DoctorInfoModel.SpecialistId = this.DoctorInfoForm.controls.Speciality.value;
+    this.DoctorInfoModel.Birthday = this.DoctorInfoForm.controls.DateOfBirth.value;
+    this.DoctorInfoModel.DoctorSubSpecialist = this.DoctorInfoForm.controls.SubSpeciality.value;
+    this.DoctorInfoModel.DoctorInfo = this.DoctorInfoForm.controls.Biography.value;
+    this.DoctorInfoModel.DoctorInfoAr = this.DoctorInfoForm.controls.BiographyAr.value;
 
-//#region SelectSpeciality Method
-SelectSpeciality(event:any){
-  this.DoctorInfoForm.controls.Speciality = event.target.value;
-}
-//#endregion
+    document.getElementById('Certificates')?.classList.add('OnClick-Style');  
+    this.CreateProfile('en',this.DoctorInfoModel);  
 
+  }
+  //#endregion
+
+  //#region SelectSpeciality Method event change
+  SelectSpeciality(event:any){
+    this.DoctorInfoForm.controls.Speciality = event.target.value;
+    this.GetSubSpecialistIdName('en', event.target.value);
+  }
+  //#endregion
+
+  //#region SubSpeciality Method event change
+  SelectSubSpeciality(event:any){
+    this.DoctorInfoForm.controls.SubSpeciality = event.target.value;
+  }
+  //#endregion
+
+  //#region SubSpeciality Method event change
+  SelectSeniority(event:any){
+    this.DoctorInfoForm.controls.Seniority = event.target.value;
+  }
+  //#endregion
+
+  //#region Countries Method event change
+  SelectCountries(event:any){
+    this.DoctorInfoForm.controls.Country = event.target.value;
+  }
+  //#endregion
+
+  //#region Gender Method event change
+  SelectGender(event:any){
+    if(event.target.value !="")
+    {
+      this.DoctorInfoForm.controls.Gender = event.target.value;
+    }
+   
+  }
+  //#endregion
+
+    //#region review AND File FormData image from input file
+    public imagePath: any;
+    imgURL: any = "../../../../assets/img/DoctorImg/Rectangle 2.png";
+    public message: string;
+  
+    preview(files:any) {
+      if (files.length === 0)
+        return;
+  
+      var mimeType = files[0].type;
+      if (mimeType.match(/image\/*/) == null) {
+        this.message = "Only images are supported.";
+        return;
+      }
+  
+      var reader = new FileReader();
+      this.imagePath = files;
+      reader.readAsDataURL(files[0]);
+      reader.onload = (_event) => {
+        this.imgURL = reader.result;
+      }
+      this.DoctorInfoModel.profileImage = files[0];
+      // this.FormDataImage.append('EpisodeIamge', files[0]);
+    }
+    //#endregion
 
 }
